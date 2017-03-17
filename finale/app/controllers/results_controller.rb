18c15b5b84ddd5cb9aac31577ae742f15d125ffe
Shereen Finney <shereenfinney@gmail.com>
2017-03-17 17:57:24 +0000

    Completed User Story 2.3
    - Wrote feature test to return group of foods causing pain
    - Change SQL query in results controller

--- finale/app/controllers/results_controller.rb
+++ finale/app/controllers/results_controller.rb
@@ -1,5 +1,7 @@
 class ResultsController < ApplicationController
   def index
-    @foodlogs = Foodlog.find_by_sql("SELECT * from foodlogs WHERE foodlogs.customdate = (SELECT customdate -1 FROM painscores WHERE score = '10')")
+    # @foodlogs = Foodlog.all
+    # @foodlogs = Foodlog.find_by_sql("SELECT * from foodlogs WHERE foodlogs.customdate = (SELECT customdate -1 FROM painscores WHERE score = '10')")
+     @foodlogs = Foodlog.find_by_sql("SELECT * FROM foodlogs JOIN painscores ON (foodlogs.customdate = (painscores.customdate -1)) WHERE painscores.score = '10'")
   end
 end
