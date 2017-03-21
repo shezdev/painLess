9d1361e4dcf54a7f12cee4a242ec67de5bc6156d
Samir Gossain <samir.gossain@gmail.com>
2017-03-21 18:14:37 +0000

    Correct Data Returns In Browser

--- finale/app/controllers/stats_controller.rb
+++ finale/app/controllers/stats_controller.rb
@@ -1,6 +1,6 @@
 class StatsController < ActionController::Base
   def index
-    @painscores = Painscore.find_by_sql("SELECT painscores.customdate, painscores.score, foodlogs.food, activities.name, activities.exertion FROM painscores, foodlogs, activities WHERE foodlogs.customdate = activities.customdate")
+    @painscores = Painscore.find_by_sql("SELECT foodlogs.food, foodlogs.customdate, painscores.score, activities.name, activities.exertion FROM foodlogs, painscores, activities WHERE foodlogs.customdate = painscores.customdate AND painscores.customdate = activities.customdate;")
   end
 end
 
