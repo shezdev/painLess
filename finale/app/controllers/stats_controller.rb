20410757854ae4cd3751433bf5ed7e382d25e52d
Shereen Finney <shereenfinney@gmail.com>
2017-03-22 09:53:34 +0000

    Adding User data and Send to GP/PDF functionality
    - Create stats controller and view
    - Add new and index method in controller
    - Add SQL query to controller to return data to build data on view
    - Update homepage view with hyperlink to see patient data
    - Add gems 'wicked_pdf' and 'wkhtmltopdf-binary'
    - Initialise wicked_pdf
    - Add PDF functionality within index controller method
    - Add PDF download link to new.html.haml
    - Rename PDF output file to index.pdf.erb

--- finale/app/controllers/stats_controller.rb
+++ finale/app/controllers/stats_controller.rb
@@ -1,10 +1,16 @@
 class StatsController < ActionController::Base
-  def index
-    @painscores = Painscore.find_by_sql("SELECT painscores.customdate, painscores.score, foodlogs.food, activities.name, activities.exertion FROM painscores, foodlogs, activities WHERE foodlogs.customdate = activities.customdate")
+  def new
+    @painscores = Painscore.find_by_sql("SELECT foodlogs.food, foodlogs.customdate, painscores.score, activities.name, activities.exertion, painscores.notes FROM foodlogs, painscores, activities WHERE foodlogs.customdate = painscores.customdate AND painscores.customdate = activities.customdate;")
   end
-end
 
-#
-# find_by_sql("SELECT painscores.customdate, painscores.score, foodlogs.food, activities.name, activities.exertion
-# FROM painscores, foodlogs, activities
-# WHERE foodlogs.customdate = activities.customdate")
+  def index
+    @painscores = Painscore.find_by_sql("SELECT foodlogs.food, foodlogs.customdate, painscores.score, activities.name, activities.exertion, painscores.notes FROM foodlogs, painscores, activities WHERE foodlogs.customdate = painscores.customdate AND painscores.customdate = activities.customdate;")
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render pdf: "file_name"   # Excluding ".pdf" extension.
+      end
+    end
+  end
+
+end
