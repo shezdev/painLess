c0ac5fbf1a85b3f38c89ca31a22a90f975856968
Shereen Finney <shereenfinney@gmail.com>
2017-03-23 13:49:40 +0000

    Changed background image and body css

--- finale/app/models/foodlog.rb
+++ finale/app/models/foodlog.rb
@@ -2,4 +2,10 @@ class Foodlog < ApplicationRecord
   validates :user_id, presence: true
   validates :food, presence: true
   belongs_to :user
+
+  #
+  # def trigger_foods_from_yesterday
+  #   find_by_sql("SELECT * FROM foodlogs JOIN painscores ON (foodlogs.customdate = (painscores.customdate -1)) WHERE painscores.score = '10'")
+  # end
+
 end
